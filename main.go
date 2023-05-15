135705280af373a273ebb4cd4ee306d11c84bdb6
duanyuntong@eventmosh.com <duanyuntong@eventmosh.com>
2023-05-15 18:18:26 +0800

    feat:init

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"meta_open_sdk/router"
-	"meta_open_sdk/task"
+	"cccn-zxl-server/router"
 
-	_ "brq5j1d.gfanx.pro/meta_cloud/meta_service/app/assets/dao"
 	"github.com/gogf/gf/frame/g"
-	"github.com/gogf/gf/os/gcfg"
-	gfnacos "github.com/imloama/gf-nacos"
 )
 
 func main() {
 	s := router.InitRouter()
 	g.Cfg().SetPath("./")
-	s.Plugin(&gfnacos.GfNacosPlugin{
-		Listener: func(configStr string) {
-			g.Log().Info("配置文件发生了更新！\n", configStr)
-			gcfg.SetContent(configStr)
-		},
-	})
 
-	go task.PaymentQueue()
 	s.Run()
 }
--- router/r.go
+++ router/r.go
@@ -1,33 +1,16 @@
 package router
 
 import (
-	apiMiddleware "meta_open_sdk/app/open/middleware"
-
-	"brq5j1d.gfanx.pro/meta_cloud/meta_common/common/middleware"
-	third "brq5j1d.gfanx.pro/meta_cloud/meta_service/app/third/middleware"
-
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
 func InitRouter() *ghttp.Server {
 	s := g.Server()
-	//跨域处理
-	s.Use(middleware.CORS)
-	s.Group("/third", func(group *ghttp.RouterGroup) {
-		group.Middleware(apiMiddleware.ApiCheck)
-		group.Middleware(third.VerifySign)
-		// group.POST("/order/create", api.OrderApi.CreateOrder)              //创建订单
-		// group.POST("/token/verify", api.UserApi.VerifyGameToken)           //校验gameToken
-		// group.POST("/user/create", api.UserApi.CreateUser)                 //创建游戏角色
-		// group.POST("/user/crystal/add", api.UserApi.AddCrystal)            //消耗钻石或红钻获得元晶
-		// group.POST("/user/inviter/info", api.UserApi.GetInviterInfo)       //查询用户邀请人信息
-		// group.POST("/user/inviter/add", api.UserApi.UpdateInviterCode)     //添加用户上级邀请人
-		// group.POST("/user/crystal/change", api.UserApi.AccountChange)      //元晶账户变动
-		// group.POST("/user/crystal/balance", api.UserApi.GetCrystalBalance) //获取元晶的余额
-	})
-	s.Group("/third/open", func(group *ghttp.RouterGroup) {
-		// group.POST("/user/loginfo/phone", api.UserApi.GetUserLoginfo) //通过手机号返回登录信息
+
+	s.BindHandler("/", func(r *ghttp.Request) {
+		r.Response.Write("Hello, World!")
+
 	})
 	return s
 }
